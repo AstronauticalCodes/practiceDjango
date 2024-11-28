2045926efd0678fc85710eb5bb9d37aefb3172be
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-11-29 00:21:29 +0530

    Final commit for blogPractice :</

--- blogPractice/blogPracSite/urls.py
+++ blogPractice/blogPracSite/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('post/<int:pk>/', views.PostView.as_view(), name='post'),
-    path('commentator/<int:pk>/', views.CommentatorView.as_view(), name='commentator'),
-    path('commentatorsList/', views.CommentatorsListView.as_view(), name='commentatorsList'),
+    # path('commentator/<int:pk>/', views.CommentatorView.as_view(), name='commentator'),
+    # path('commentatorsList/', views.CommentatorsListView.as_view(), name='commentatorsList'),
+    # commentig.. declaring end of support for this project of django. Thanks for learning.
 ]
